28004f2d42a1044d4544e4ac3ee149ab1606680f
mikle006 <kurch7815@gmail.com>
2021-06-01 07:48:13 +0300

    comit plz

---
+++ Assets/Materials/scripts/HeroeScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroeScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f; 
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForce = 15f;
+
+
+    private Rigidbody2D rb;
+    private SpriteRender sprite;
+
+    private void Awake()
+    {
+        rb = GetCompanent<Rigidbody2D>();
+        sprite = GetCompanentInChildren<SpriteRender>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.Getbutton("Horizontal"))
+            Run();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transforme.right * Input.GetAxis("Horizontal");
+
+        transforme.position = Vector3.MoveTowards(transforme.position, transforme.position + dir, speed * Time.deltaTime) ;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Petproject
+# Pet project\Assets\Scenes и открываете файл с расширением scen
  
